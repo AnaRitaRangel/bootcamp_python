dc8e5f60e0f6766258189315dc2fd633fb549803
AnaRitaRangel <106035404+AnaRitaRangel@users.noreply.github.com>
2024-08-21 19:36:24 -0300

    Meu primeiro commit da aula 2 do bootcamp de python

---
+++ exercicios.py
@@ -0,0 +1,15 @@
+#1 Escreva um programa que soma dois números inteiros inseridos pelo usuário
+
+print("Vamos somar ?")
+
+primeiro_numero = int(input("Digite um número inteiro: "))
+
+segundo_numero = int(input("Digite outro número inteiro: "))
+
+soma = primeiro_numero + segundo_numero
+
+print(f"A soma entre {primeiro_numero} e {segundo_numero} é igual a {soma}")
+
+
+
+
