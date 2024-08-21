bae2e495a18f428f21c18ab7ba4cc9696d7f8d94
AnaRitaRangel <106035404+AnaRitaRangel@users.noreply.github.com>
2024-08-21 20:50:16 -0300

    Adiciona os exercícios 2 a 4

--- exercicios.py
+++ exercicios.py
@@ -13,3 +13,41 @@ print(f"A soma entre {primeiro_numero} e {segundo_numero} é igual a {soma}")
 
 
 
+# 2.	Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5
+
+print("Vamos calcular o resto da divisão de um número qualquer por 5 ?")
+
+numero = float(input("Digite um número para dividirmos por 5: "))
+
+resto = numero % 5
+
+print(f"O resto da divisão de {numero} por 5 é igual a {resto}")
+
+
+# 3.	Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado
+
+print("Que tal fazermos a multiplicação de dois números ?")
+
+primeiro_numero = int(input("Digite um número inteiro: "))
+
+segundo_numero = int(input("Digite outro número inteiro: "))
+
+mult = primeiro_numero * segundo_numero
+
+print(f"O resultado da multiplicação entre {primeiro_numero} e {segundo_numero} é igual a {mult}")
+
+
+# 4.	Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo
+
+print("Agora vamos nos divertir fazendo uma divisão ?")
+
+dividendo = int(input("Digite o número a ser dividido (dividendo) : "))
+
+divisor = int(input("Agora digite o número para dividir o anterior (divisor) : "))
+
+quociente = dividendo // divisor
+
+print(f"O resultado da divisão inteira entre {dividendo} e {divisor} é igual a {quociente}")
+
+
+
