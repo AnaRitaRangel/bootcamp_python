6e24fa25a357178849f3500ec3d310299c54f5e6
AnaRitaRangel <106035404+AnaRitaRangel@users.noreply.github.com>
2024-08-25 13:48:15 -0300

    “arquivo-de-TypeError-dia-25”

--- erros.py
+++ erros.py
@@ -1,18 +1,26 @@
-# 4.    Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo
-
-try:
-    print("Agora vamos nos divertir fazendo uma divisão ?")
-    dividendo = int(input("Digite o número a ser dividido (dividendo) : "))
-    divisor = int(input("Agora digite o número para dividir o anterior (divisor) : "))
-    quociente = dividendo // divisor
-    print(f"O resultado da divisão inteira entre {dividendo} e {divisor} é igual a {quociente}")
-except:ZeroDivisionError
-print("integer division or modulo by zero")
-
-# # Exemplo que causa TypeError
+# Exemplo que causa TypeError
 try:
     resultado = len(5)
 except TypeError as e:
-    print(e)
+    print(e)  # Imprime a mensagem de erro
+
+
+# Exemplo de Type Check
+numero = 10
+if isinstance(numero, int):
+    print("A variável é um inteiro.")
+else:
+    print("A variável não é um inteiro.")
+
+# Exemplo Try/Except/Else
+try:
+    resultado = len("Ana")
+except TypeError as e:
+    print(e)  # Imprime a mensagem de erro
+else:
+    print("Tudo correu bem")
+finally
+    print("O importante é participar")
+
 
 
