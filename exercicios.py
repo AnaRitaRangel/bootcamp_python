7963ed211542cd7f5221ce6b841bd018b019000b
AnaRitaRangel <106035404+AnaRitaRangel@users.noreply.github.com>
2024-08-28 19:21:13 -0300

    exercicios resolvido o problema de try except

--- exercicios.py
+++ exercicios.py
@@ -38,16 +38,14 @@ print(f"O resultado da multiplicação entre {primeiro_numero} e {segundo_numero
 
 
 # 4.    Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo
-
-print("Agora vamos nos divertir fazendo uma divisão ?")
-
-dividendo = int(input("Digite o número a ser dividido (dividendo) : "))
-
-	divisor = int(input("Agora digite o número para dividir o anterior (divisor) : "))
-
-quociente = dividendo // divisor
-
-print(f"O resultado da divisão inteira entre {dividendo} e {divisor} é igual a {quociente}")
+try:
+    print("Agora vamos nos divertir fazendo uma divisão ?")
+    dividendo = int(input("Digite o número a ser dividido (dividendo) : "))
+    divisor = int(input("Agora digite o número para dividir o anterior (divisor) : "))
+    quociente = dividendo // divisor
+    print(f"O resultado da divisão inteira entre {dividendo} e {divisor} é igual a {quociente}")
+except ZeroDivisionError:
+    print("integer division or modulo by zero")
 
 
 # 5.    Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
